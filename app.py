04a58ac36c9ab34d08af8a4171e252f21c85d320
shamansk1y <shamansky1225@gmail.com>
2023-05-18 00:19:16 +0200

    add index.html, result.html upd app.py

--- app.py
+++ app.py
@@ -2,18 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from fake_useragent import UserAgent
 from parsing import *
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
 def main():
+    item = request.form['item']
     ua = UserAgent().random
     headers = {'User-Agent': ua}
 
     chrome_driver_path = r"E:\development prog\chromedriver.exe"
     service = Service(chrome_driver_path)
     driver = webdriver.Chrome(service=service)
-
-    item = 'MUNW2201TF'
-
     result = {}
 
     result['copa.com.ua'] = get_copa_price(item, headers)
@@ -23,10 +28,13 @@ def main():
     result['playfootball.com.ua'] = get_playfootball_price(item, headers)
     result['soccer-shop.com.ua'] = get_soccer_shop_price(item, headers)
     result['rozetka.com.ua'] = get_rozetka_price(item, driver)
+    result['epicentrk.ua'] = get_epic_price(item, headers)
+    result['kasta.ua'] = get_kasta_price(item, headers)
+    result['eobuv.com.ua'] = get_eobuv_price(item, headers)
 
     driver.quit()
 
-    print(result)
+    return result
 
 if __name__ == "__main__":
-    main()
+    app()
--- parsing.py
+++ parsing.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 
 
 def get_copa_price(item, headers):
-    link = 'https://www.copa.com.ua//index.php?route=product/search&search=' + item
+    link = f'https://www.copa.com.ua//index.php?route=product/search&search={item}'
     response = requests.get(link, headers=headers).text
     soup = BeautifulSoup(response, 'lxml')
     price_element = soup.find(class_='price')
@@ -20,7 +20,7 @@ def get_copa_price(item, headers):
         return "Цена не найдена"
 
 def get_hunter_price(item, headers):
-    link = 'https://sport-hunter.com.ua/uk/index.php?route=product/search&search=' + item
+    link = f'https://sport-hunter.com.ua/uk/index.php?route=product/search&search={item}'
     response = requests.get(link, headers=headers).text
     soup = BeautifulSoup(response, 'lxml')
     price_element = soup.find(class_='product-card__price')
@@ -50,14 +50,14 @@ def get_korzina_price(item, headers, driver):
     href = first_result_link["href"]
     response = requests.get(href, headers=headers).text
     soup = BeautifulSoup(response, "html.parser")
-    price_element = soup.find_all("span", class_="hikashop_product_price hikashop_product_price_0")[1].text
+    price_element = soup.find_all("span", class_="hikashop_product_price hikashop_product_price_0 hikashop_product_price_with_discount")[1].text
     if price_element:
         return price_element
     else:
         return "Цена не найдена"
 
 def get_korzina_2_price(item, headers):
-    link = 'https://football-world.com.ua/ru/search?search=' + item
+    link = f'https://football-world.com.ua/ru/search?search={item}'
     response = requests.get(link, headers=headers).text
     soup = BeautifulSoup(response, 'lxml')
     price_element = soup.find('div', class_='product-cart__price-current product-cart__price-current--red')
@@ -72,7 +72,7 @@ def get_korzina_2_price(item, headers):
     return price
 
 def get_playfootball_price(item, headers):
-    link = 'https://playfootball.com.ua/shop/search?text=' + item
+    link = f'https://playfootball.com.ua/shop/search?text={item}'
     response = requests.get(link, headers=headers).text
     soup = BeautifulSoup(response, 'lxml')
     price_element = soup.find('span', class_='product-price__main-value')
@@ -84,7 +84,7 @@ def get_playfootball_price(item, headers):
     return price
 
 def get_soccer_shop_price(item, headers):
-    link = 'https://soccer-shop.com.ua/ua/advanced_search_result.php?keywords=' + item
+    link = f'https://soccer-shop.com.ua/ua/advanced_search_result.php?keywords={item}'
     response = requests.get(link, headers=headers).text
     soup = BeautifulSoup(response, 'lxml')
     price_element = soup.find('span', class_='price sale')
@@ -97,7 +97,7 @@ def get_soccer_shop_price(item, headers):
     return price
 
 def get_rozetka_price(item, driver):
-    driver.get("https://rozetka.com.ua/search/?text=" + item)
+    driver.get(f"https://rozetka.com.ua/search/?text={item}")
     time.sleep(7)
     html = driver.page_source
     soup = BeautifulSoup(html, 'lxml')
@@ -106,6 +106,44 @@ def get_rozetka_price(item, driver):
     if price_element:
         price = price_element.get_text(strip=True)
         price = price.replace('\xa0', '')
+    else:
+        price = "Цена не найдена"
+    return price
+
+
+def get_epic_price(item, headers):
+    link = f'https://epicentrk.ua/ua/search/?q={item}&sort=asc'
+    response = requests.get(link, headers=headers).text
+    soup = BeautifulSoup(response, 'lxml')
+    price_element = soup.find('span', class_='card__price-sum')
+    if price_element:
+        price = price_element.get_text(strip=True)
+    else:
+        price = "Цена не найдена"
+    return price
+
+
+def get_kasta_price(item, headers):
+    link = f'https://kasta.ua/uk/search/?q={item}&sort=price-asc'
+    response = requests.get(link, headers=headers).text
+    soup = BeautifulSoup(response, 'lxml')
+    price_element = soup.find('span', class_='product_item__new-cost')
+    if price_element:
+        string = price_element.get_text(strip=True)
+        price = string.encode('latin-1').decode('utf-8')
+    else:
+        price = "Цена не найдена"
+    return price
+
+
+def get_eobuv_price(item, headers):
+    link = f'https://eobuv.com.ua/s/{item}?q={item}'
+    response = requests.get(link, headers=headers).text
+    soup = BeautifulSoup(response, 'lxml')
+    price_element = soup.find('div', class_='price-final')
+    if price_element:
+        string = price_element.get_text(strip=True)
+        price = string.replace('\xa0', ' ').strip()
     else:
         price = "Цена не найдена"
     return price

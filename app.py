feb532ac99200af5e82eeb1ac8588f9a0cfdf2c5
shamansk1y <shamansky1225@gmail.com>
2023-05-18 13:18:01 +0200

    upd

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from fake_useragent import UserAgent
 from parsing import *
 from flask import Flask, render_template, request
-import os
 
 app = Flask(__name__)
 
@@ -11,21 +10,17 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
 
-chrome_path = os.environ.get("GOOGLE_CHROME_BIN", "chromedriver")
-
-driver = webdriver.Chrome(executable_path=chrome_path, options=chrome_options)
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/result', methods=['POST'])
 def main():
     item = request.form['item']
     ua = UserAgent().random
     headers = {'User-Agent': ua}
+
+    driver = webdriver.Chrome(options=chrome_options)
 
     result = {}
     result['copa.com.ua'] = get_copa_price(item, headers)
@@ -38,6 +33,8 @@ def main():
     result['epicentrk.ua'] = get_epic_price(item, headers)
     result['kasta.ua'] = get_kasta_price(item, headers)
     result['eobuv.com.ua'] = get_eobuv_price(item, headers)
+
+    driver.quit()
 
     return render_template('result.html', result=result, item=item)
 

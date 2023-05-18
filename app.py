6554f69072495533ba9caeb6ed6531a4ca867e95
shamansk1y <shamansky1225@gmail.com>
2023-05-18 13:00:03 +0200

    upd

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ from selenium import webdriver
 from fake_useragent import UserAgent
 from parsing import *
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--no-sandbox')
+
+chrome_path = os.environ.get("GOOGLE_CHROME_BIN", "chromedriver")
+
+driver = webdriver.Chrome(executable_path=chrome_path, options=chrome_options)
 
 
 @app.route('/')
@@ -17,13 +27,6 @@ def main():
     ua = UserAgent().random
     headers = {'User-Agent': ua}
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--no-sandbox')
-
-    driver = webdriver.Chrome(options=chrome_options)
-
     result = {}
     result['copa.com.ua'] = get_copa_price(item, headers)
     result['sport-hunter.com.ua'] = get_hunter_price(item, headers)
@@ -35,8 +38,6 @@ def main():
     result['epicentrk.ua'] = get_epic_price(item, headers)
     result['kasta.ua'] = get_kasta_price(item, headers)
     result['eobuv.com.ua'] = get_eobuv_price(item, headers)
-
-    driver.quit()
 
     return render_template('result.html', result=result, item=item)
 
